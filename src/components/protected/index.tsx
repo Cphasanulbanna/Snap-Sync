8b8eca478d181f9ec82d8d743d49746d21d19cf6
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-19 19:25:27 +0530

    firebase auth

--- src/components/protected/index.tsx
+++ src/components/protected/index.tsx
@@ -1,13 +1,21 @@
 import {  useUserAuth } from '@/context/userAuthContext';
+import { getAuth } from 'firebase/auth';
 import * as React from 'react';
 import { Navigate, useLocation, Outlet } from 'react-router-dom';
+import {useAuthState} from "react-firebase-hooks/auth"
 
 interface IProtectedRoutesProps {
 }
 
 const ProtectedRoutes: React.FunctionComponent<IProtectedRoutesProps> = (props) => {
     const location = useLocation()
-    const {user} = useUserAuth()
+    const auth  =getAuth()
+    const [user, loading] = useAuthState(auth)
+
+    if(loading) {
+      return <div>Loading...</div>
+    }
+
   return  user ? <Outlet />: <Navigate to="/login" state={{from: location }}/>
 };
 
