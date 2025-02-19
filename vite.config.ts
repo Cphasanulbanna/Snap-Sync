bd31d8cedc2e7f7a8dd3a34870ae92234cae3ee8
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-19 17:36:16 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+    port: 3000
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    open: true,
-    port: 3000
   }
 })
