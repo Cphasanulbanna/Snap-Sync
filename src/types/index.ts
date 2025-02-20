e23c6f44860d23f1a2077e70b13580cde822e371
Cphasanulbanna <cphasanulbanna@gmail.com>
2025-02-20 12:20:45 +0530

    created types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { OutputFileEntry } from "@uploadcare/react-uploader"
+
 export interface UserLogIn {
     email: string,
     password: string
@@ -7,4 +9,22 @@ export interface UserSignIn {
     email: string,
     password: string,
     confirmPassword: string
+}
+
+export interface FileEntry {
+    files: OutputFileEntry[]
+}
+
+export interface PhotoMeta {
+    cdnUrl: string,
+    uuid: string
+}
+
+export interface Post {
+    captions: string,
+    photo: PhotoMeta[],
+    likes: number,
+    userLikes: [],
+    userId: string | null,
+    date: Date
 }
